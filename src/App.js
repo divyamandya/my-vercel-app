07e3482f81d6c10330832ecd476885dd82790366
divyamandya <divyamandya@gmail.com>
2026-03-12 15:07:45 +0530

    API layesr added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Hello from React 🚀</h1>
+      <h1>Hello from React </h1>
       <p>Updated the content</p>
     </div>
   );
